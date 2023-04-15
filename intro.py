da4853a4ea359e2acaff754be27dffc43db02904
coderkarunakar <chembetikarunakar@gmail.com>
2023-04-15 19:38:18 +0530

    node of linked list

--- intro.py
+++ intro.py
@@ -4,7 +4,7 @@
 # 2.all the elements are connected in non continous manner
 #3.there will a link btw the elements
 #4.each element of the linked list is known as node
-#5.node will be storing 2 things i.e 
+#5.node will be storing 2 things i.e
    #i.data of the current value
    #ii. reference to the next node value
   #EXAMPLE:CONSTRUCTION OF LINKED LIST WITH THE GIVEN VALUES
@@ -34,16 +34,59 @@
 #i. head of the linked list stores the reference to the first node
 #ii. Tail of the linked list stores the reference of the last node in the linked list
 #iii. maximum we deal with head only not much with tail (very less)
-#note:if we have ref of the head of the linked list then we have a complete linked list,because 
+#note:if we have ref of the head of the linked list then we have a complete linked list,because
 #with that only we can move to last node in the linked list
 
 
 #EXAMPLE:
 
 # DATA  REF  ....
-# 1     400         5  600    4 800   3  1000   
+# 1     400         5  600    4 800   3  1000
 
-                                    #   2   none     
+                                    #   2   none
 
 #if we have  a head of the linked list then we have a complete linked list
-  
+
+
+
+
+
+          #Linked List Node: some coding part
+#a linked list is a collection of nodes
+#every node has its data value and next data reference node
+#if no data is present then next reference is none
+#imp note:every node has 2 things i.e data(current value) and next (i.e ref of next node)
+#note:when ever we want to initialize a node we provide data with that
+#if we create data then by default automatically next is none
+
+
+
+
+
+
+class Node:
+   def __init__(self,data):
+      self.data=data
+      self.next=None
+a=Node(13)
+b=Node(15)
+a.next=b
+    
+print(a.data)#13
+print(b.data)#15
+print(a.next.data)#15 because a.next.data  here next data refers to the b so it is 15(first it goes to a i.e 13 then next.data i.e 15)
+print(a)
+print(a.next)
+print(b)
+# print (b.next.data) this gives error none type has no attribute data it means it stores it data value and next ref i.e none so its next ref is not there so we get and error here
+
+    
+    
+ 
+
+
+ 
+
+
+
+
